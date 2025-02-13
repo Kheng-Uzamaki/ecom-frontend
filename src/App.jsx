c3a8f9e22d34a8c7819b74a6f82507f7957090be
Kheng <khenguzamiki1@gmail.com>
2025-02-13 10:37:50 +0700

    setup-project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { FaBeer } from 'react-icons/fa'
+import './index.css'
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-800 text-2xl font-bold text-white'>
+      Welcome <FaBeer/>
+
+    </div>
+  )
+}
+
+export default App
