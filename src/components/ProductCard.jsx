df27decc7e3ff0f285a89bcb0258d6a31865ad91
Kheng <khenguzamiki1@gmail.com>
2025-02-14 14:37:37 +0700

    truncateText

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
 import ProductViewModal from './ProductViewModal';
+import truncateText from './truncateText';
 
 const ProductCard = ({ productId, productName, image, description, quantity, price, discount, specialPrice }) => {
   const [openProductViewModal, setOpenProductViewModal] = useState(false);
@@ -15,7 +16,18 @@ const ProductCard = ({ productId, productName, image, description, quantity, pri
   return (
     <div className="border-none rounded-lg shadow-lg overflow-hidden transition-shadow duration-300">
       <div
-        onClick={() => handleProductView({ productId, productName, image, description, quantity, price, discount, specialPrice })}
+        onClick={() =>
+          handleProductView({
+            productId,
+            productName,
+            image,
+            description,
+            quantity,
+            price,
+            discount,
+            specialPrice,
+          })
+        }
         className=" w-full overflow-hidden aspect-[3/2]"
       >
         <img
@@ -26,34 +38,60 @@ const ProductCard = ({ productId, productName, image, description, quantity, pri
       </div>
       <div className="p-4">
         <h2
-          onClick={() => handleProductView({ productId, productName, image, description, quantity, price, discount, specialPrice })}
+          onClick={() =>
+            handleProductView({
+              productId,
+              productName,
+              image,
+              description,
+              quantity,
+              price,
+              discount,
+              specialPrice,
+            })
+          }
           className="text-lg font-semibold cursor-pointer"
         >
-          {productName}
+          {truncateText(productName, 50)}
         </h2>
         <div className="min-h-20 max-h-20">
-          <p className="text-gray-600 text-sm">{description}</p>
+          <p className="text-gray-600 text-sm">{
+            truncateText(description, 90)
+          }</p>
         </div>
         <div className="flex justify-between items-center">
           {specialPrice ? (
             <div className="flex flex-col">
-              <span className="text-gray-400 line-through">${Number(price).toFixed(2)}</span>
-              <span className="text-slate-700 font-bold text-xl">${Number(specialPrice).toFixed(2)}</span>
+              <span className="text-gray-400 line-through">
+                ${Number(price).toFixed(2)}
+              </span>
+              <span className="text-slate-700 font-bold text-xl">
+                ${Number(specialPrice).toFixed(2)}
+              </span>
             </div>
           ) : (
-            <span className="text-slate-700 font-bold text-xl">${Number(price).toFixed(2)}</span>
+            <span className="text-slate-700 font-bold text-xl">
+              ${Number(price).toFixed(2)}
+            </span>
           )}
           <button
             onClick={() => {}}
             disabled={!isAvailable}
-            className={`bg-blue-500 ${isAvailable ? 'opacity-100 hover:bg-blue-600' : 'opacity-60'} text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center cursor-pointer`}
+            className={`bg-blue-500 ${
+              isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-60"
+            } text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center cursor-pointer`}
           >
             <FaShoppingCart className="mr-2" />
-            {isAvailable ? 'Add to Cart' : 'Stock Out'}
+            {isAvailable ? "Add to Cart" : "Stock Out"}
           </button>
         </div>
       </div>
-      <ProductViewModal open={openProductViewModal} setOpen={setOpenProductViewModal} product={selectedViewProduct} isAvailable={isAvailable} />
+      <ProductViewModal
+        open={openProductViewModal}
+        setOpen={setOpenProductViewModal}
+        product={selectedViewProduct}
+        isAvailable={isAvailable}
+      />
     </div>
   );
 };
