27835b4837a3459341f5cc26a31937855446fac8
Kheng <khenguzamiki1@gmail.com>
2025-02-14 15:02:04 +0700

    setting up the routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@
 import { FaBeer } from 'react-icons/fa'
 import './index.css'
 import Products from './components/Products/Products'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './components/home/Home'
 function App() {
   return (
-    <div className="">
-      <Products />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
   )
 }
 
